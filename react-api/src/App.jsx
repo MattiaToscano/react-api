fd3a7c4040dde7453f08658212aaa3421c6611db
MattiaToscano <mattia1997ct@gmail.com>
2025-05-13 13:18:30 +0200

    [UPD] Effettuata chiamata, creata struttura base

--- react-api/src/App.jsx
+++ react-api/src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 function App() {
+  const [actress, setActress] = useState([]);
 
+  const fetchActress = () => {
+    axios.get('https://api.themoviedb.org/3/person/popular')
+      .then(response => {
+        setActress(response.data.results);
+      })
+      .catch(error) => {
+    console.log(error);
+  }
+
+  useEffect(() => {
+    fetchActress();
+  }, []);
 
   return (
     <>
-
+      <div className="container">
+        {actress.map((person) => (
+          <div key={person.id}>
+            {console.log(person)}
+            <p>{person.name}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
